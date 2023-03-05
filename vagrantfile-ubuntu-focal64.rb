2761a98781b148a4936bb96bb9f723d9f4a7a672
João Pedro Hagen <108746755+joaopedrohagen@users.noreply.github.com>
2023-03-05 17:39:28 -0300

    Add Vagrantfile for 4 VMs with Docker, Swarm manager and workers
    Added a Vagrantfile that defines 4 virtual machines with fixed IP addresses, all with Docker installed. The 'Master' machine is defined as the Swarm manager node, and the other machines are added to the Swarm cluster as workers

--- vagrantfile-ubuntu-focal64.rb
+++ vagrantfile-ubuntu-focal64.rb
@@ -1,66 +1,50 @@
-# Versão do Vagrant que será utilizada
 Vagrant.configure("2") do |config|
-
-  # Selecionando o box que será utilizado
+  
+  # Define a box a ser usada para as máquinas virtuais
   config.vm.box = "ubuntu/focal64"
-
-  # Definindo as configurações da máquina virtual 1
-  config.vm.define "vm1" do |vm1|
-    vm1.vm.hostname = "vm1"
-    vm1.vm.network "public_network", bridge: "en0: Wi-Fi (AirPort)", auto_config: true
-    vm1.vm.provider "virtualbox" do |vb|
-      vb.memory = "1024" # Definindo a quantidade de RAM
-      vb.cpus = 1 # Definindo o número de CPUs
-    end
-    # Provisão para instalação do Docker
-    vm1.vm.provision "shell", inline: <<-SHELL
+  
+  # Define a máquina "Master"
+  config.vm.define "Master" do |master|
+    master.vm.hostname = "Master"
+    master.vm.network "private_network", ip: "192.168.50.10"
+    master.vm.provision "shell", inline: <<-SHELL
       apt-get update
       apt-get install -y docker.io
+      docker swarm init --advertise-addr 192.168.50.10
     SHELL
   end
-
-  # Definindo as configurações da máquina virtual 2
-  config.vm.define "vm2" do |vm2|
-    vm2.vm.hostname = "vm2"
-    vm2.vm.network "public_network", bridge: "en0: Wi-Fi (AirPort)", auto_config: true
-    vm2.vm.provider "virtualbox" do |vb|
-      vb.memory = "1024" # Definindo a quantidade de RAM
-      vb.cpus = 1 # Definindo o número de CPUs
-    end
-    # Provisão para instalação do Docker
-    vm2.vm.provision "shell", inline: <<-SHELL
+  
+  # Define a máquina "Node01"
+  config.vm.define "Node01" do |node01|
+    node01.vm.hostname = "Node01"
+    node01.vm.network "private_network", ip: "192.168.50.11"
+    node01.vm.provision "shell", inline: <<-SHELL
       apt-get update
       apt-get install -y docker.io
+      docker swarm join --token <token> 192.168.50.10:2377
     SHELL
   end
-
-  # Definindo as configurações da máquina virtual 3
-  config.vm.define "vm3" do |vm3|
-    vm3.vm.hostname = "vm3"
-    vm3.vm.network "public_network", bridge: "en0: Wi-Fi (AirPort)", auto_config: true
-    vm3.vm.provider "virtualbox" do |vb|
-      vb.memory = "1024" # Definindo a quantidade de RAM
-      vb.cpus = 1 # Definindo o número de CPUs
-    end
-    # Provisão para instalação do Docker
-    vm3.vm.provision "shell", inline: <<-SHELL
+  
+  # Define a máquina "Node02"
+  config.vm.define "Node02" do |node02|
+    node02.vm.hostname = "Node02"
+    node02.vm.network "private_network", ip: "192.168.50.12"
+    node02.vm.provision "shell", inline: <<-SHELL
       apt-get update
       apt-get install -y docker.io
+      docker swarm join --token <token> 192.168.50.10:2377
     SHELL
   end
-
-  # Definindo as configurações da máquina virtual 4
-  config.vm.define "vm4" do |vm4|
-    vm4.vm.hostname = "vm4"
-    vm4.vm.network "public_network", bridge: "en0: Wi-Fi (AirPort)", auto_config: true
-    vm4.vm.provider "virtualbox" do |vb|
-      vb.memory = "1024" # Definindo a quantidade de RAM
-      vb.cpus = 1 # Definindo o número de CPUs
-    end
-    # Provisão para instalação do Docker
-    vm4.vm.provision "shell", inline: <<-SHELL
+  
+  # Define a máquina "Node03"
+  config.vm.define "Node03" do |node03|
+    node03.vm.hostname = "Node03"
+    node03.vm.network "private_network", ip: "192.168.50.13"
+    node03.vm.provision "shell", inline: <<-SHELL
       apt-get update
       apt-get install -y docker.io
+      docker swarm join --token <token> 192.168.50.10:2377
     SHELL
   end
+  
 end
